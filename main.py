b7b6403dc8b85225fb1076a01156dde1819c2d55
angrypigs <angrypigs.tnt@gmail.com>
2023-10-02 20:38:34 +0200

    listbox widget

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pynput import mouse, keyboard
 import pyautogui as pg
 import tkinter as tk
 import customtkinter as ctk
+from CTkListbox import *
 import time
 
 class App:
@@ -13,8 +14,16 @@ class App:
         self.master.geometry(f"{self.WIDTH}x{self.HEIGHT}")
         self.master.title("Macro maker")
         self.master.resizable(False, False)
-
+        self.init_menu()
         self.master.mainloop()
+
+    def init_menu(self) -> None:
+        self.commands_frame = ctk.CTkFrame(self.master, height=self.HEIGHT-100,
+            width=300)
+        self.commands_frame.place(relx=0.2, rely=0.5, anchor=tk.CENTER)
+        self.commands_tablist = CTkListbox(self.commands_frame, width=240,
+                                           height=self.HEIGHT-160)
+        self.commands_tablist.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 if __name__ == "__main__":
     App()
