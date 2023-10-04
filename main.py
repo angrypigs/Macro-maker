f612a9f944c37becb4a6f7cb61240e773fc881ea
angrypigs <angrypigs.tnt@gmail.com>
2023-10-05 00:34:24 +0200

    start of creating listbox class

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 from pynput import mouse, keyboard
 import pyautogui as pg
 import customtkinter as ctk
-from CTkListbox import *
-import time
+import tkinter as tk
+import time as tm
+
+class CTkListbox:
+
+    def __init__(self, master, width: int, height: int) -> None:
+        self.surf = ctk.CTkScrollableFrame(master, width=width, height=height)
+        self.H = height
+        self.W = width
+        self.cells = []
+    
+    def __create_new(self, text: str) -> None:
+        button = ctk.CTkButton(self.surf, width=self.W-40, height=30, text=text)
+        button.grid(row=len(self.cells)-1, column=0, padx=10, pady=5)
+
+    def __reset_by_index(self, index: int) -> None:
+        for i in range(index, len(self.cells)):
+            gridcell = self.surf.grid_slaves(i, 0)
+            if len(gridcell)==0:
+                self.__create_new(self.cells[i])
+            else:
+                gridcell[0].configure(text=self.cells[i])
+
+    def insert(self, index: int= -1, text: str = "") -> None:
+        if index == -1:
+            self.cells.append(text)
+            self.__create_new(text)
+        else:
+            self.cells.insert(index, text)
+            self.__reset_by_index(index)
+            
+    def place(self, x: int, y: int) -> None:
+        self.surf.place(x=x,y=y)
+
+    
 
 class App:
 
@@ -13,17 +46,46 @@ class App:
         self.master.geometry(f"{self.WIDTH}x{self.HEIGHT}")
         self.master.title("Macro maker")
         self.master.resizable(False, False)
+        self.time_value = tk.StringVar(self.master, "")
+        self.commands_list = []
         self.init_menu()
         self.master.mainloop()
 
+    def push_time_command(self) -> None:
+        """
+        
+        """
+        self.commands_tablist.insert(2, "abc")
+        # try:
+        #     time = float(self.time_value.get())
+        # except Exception:
+        #     time = 1
+        # if time == int(time): time = int(time)
+        # index = self.commands_tablist.curselection()
+        # if index==None:
+        #     self.commands_list.append(f"time {time}")
+        #     self.commands_tablist.insert(tk.END, f"Time ({time})")
+        # else:
+        #     print(index)
+        #     self.commands_list.insert(index, f"time {time}")
+        #     self.commands_tablist.insert(6, f"Time ({time})")
+        # print(self.commands_list)
+
     def init_menu(self) -> None:
+
+        self.commands_tablist = CTkListbox(self.master, 300, 600)
+        self.commands_tablist.place(50, 50)
+        for i in range(20):
+            self.commands_tablist.insert(i, f"{i}")
+
         # command list frame
-        self.commands_frame = ctk.CTkFrame(self.master, height=self.HEIGHT-80,
-            width=self.WIDTH//2-80, corner_radius=10)
-        self.commands_frame.place(relx=0.25, rely=0.5, anchor='center')
-        self.commands_tablist = CTkListbox(self.commands_frame, 
-            width=self.WIDTH//2-160, height=self.HEIGHT-160)
-        self.commands_tablist.place(relx=0.5, rely=0.5, anchor='center')
+        # self.commands_frame = ctk.CTkFrame(self.master, height=self.HEIGHT-80,
+        #     width=self.WIDTH//2-80, corner_radius=10)
+        # self.commands_frame.place(relx=0.25, rely=0.5, anchor='center')
+        # self.commands_tablist = CTkListbox(self.commands_frame, 
+        #     width=self.WIDTH//2-160, height=self.HEIGHT-160)
+        # self.commands_tablist.place(relx=0.5, rely=0.5, anchor='center')
+
         # title frame
         self.title_frame = ctk.CTkFrame(self.master, width=self.WIDTH//2-80,
             height=80, corner_radius=10)
@@ -31,18 +93,31 @@ class App:
         ctk.CTkLabel(self.title_frame, height=60, width=self.WIDTH//2-80,
                      font=("Roboto", 32), text="Macro maker"
                      ).place(relx=0.5, rely=0.5, anchor='center')
+        
         # options frame
         self.options_frame = ctk.CTkFrame(self.master, width=self.WIDTH//2-80,
             height=160, corner_radius=10)
         self.options_frame.place(relx=0.75, rely=0.32, anchor='center')
+
         # time frame
         self.time_frame = ctk.CTkFrame(self.master, width=self.WIDTH//2-80,
             height=120, corner_radius=10)
         self.time_frame.place(relx=0.75, rely=0.56, anchor='center')
+        ctk.CTkEntry(self.time_frame, width=180, height=30, 
+                     textvariable=self.time_value, font=("Roboto", 18)).place(
+                         relx=0.5, rely=0.3, anchor=tk.CENTER)
+        ctk.CTkButton(self.time_frame, width=180, height=30,
+                      font=("Roboto", 18), text="Add time command",
+                      command=self.push_time_command).place(
+                         relx=0.5, rely=0.7, anchor='center') 
+
         # save frame
         self.save_frame = ctk.CTkFrame(self.master, width=self.WIDTH//2-80,
             height=80, corner_radius=10)
         self.save_frame.place(relx=0.75, rely=0.74, anchor='center')
+
+        
+
         # notes frame
         self.notes_frame = ctk.CTkFrame(self.master, width=self.WIDTH//2-80,
             height=80, corner_radius=10)
